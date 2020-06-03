82924af944b28ab6b473d07bf39deab5f8300eeb
RyoTanaka0531 <ch5cxcxxh@gmail.com>
2020-06-03 14:38:06 +0000

    update views

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,8 @@ class Product < ApplicationRecord
     belongs_to :category
     has_many :orders
     attachment :product_image
+    validates :name, presence: true
+    validates :price, presence: true
+    validates :timing, presence: true
+    validates :description, presence: true
 end
