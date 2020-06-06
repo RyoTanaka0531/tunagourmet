fd003b813500e67cf0375b5bbe55e62a32071ff6
RyoTanaka0531 <ch5cxcxxh@gmail.com>
2020-06-06 06:26:50 +0000

    edit order_product

--- app/controllers/buyer/order_products_controller.rb
+++ app/controllers/buyer/order_products_controller.rb
@@ -1,26 +1,27 @@
 class Buyer::OrderProductsController < ApplicationController
 
-  def create
-    @order_product = OrderProduct.new(order_product_params)
-    @order_product.save
-    #リダイレクト先はorders#newかorders#confirm
-    redirect_to root_path
-end
+    def create
+        @order_product = OrderProduct.new(order_product_params)
+        @order_product.save
+        #リダイレクト先はorders#newかorders#confirm
+        redirect_to root_path
+    end
 
-def destroy
-    @order_product = OrderProduct.find(params[:id])
-    @order_product.destroy
-    redirect_to root_path
-end
 
-def new
-    @order_product = OrderProduct.new
-    @product = Product.find(params[:id])
-end
+    def new
+        @order_product = OrderProduct.new
+        @product = Product.find(params[:id])
+    end
 
-private
-def order_product_params
-    require.params(:order_product).permit(:count, :payment, :product_id, :order_id)
-end
+    def destroy
+        @order_product = OrderProduct.find(params[:id])
+        @order_product.destroy
+        redirect_to root_path
+    end
+
+    private
+    def order_product_params
+        params.require(:order_product).permit(:count, :payment, :product_id, :order_id, :postage)
+    end
 end
 
