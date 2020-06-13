d1126f86f66121695daa4928fdc673fcf36354f9
RyoTanaka0531 <ch5cxcxxh@gmail.com>
2020-06-13 12:19:22 +0000

    [update]migrate

--- db/migrate/20200529072424_create_messages.rb
+++ db/migrate/20200529072424_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
-      t.references :room, foreign_key: true
+      #t.references :room, foreign_key: true
       t.boolean :is_producer
       t.text :content
 
