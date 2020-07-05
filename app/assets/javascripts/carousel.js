d8767711be533a7ba3f08378b99ee028acad5bee
RyoTanaka0531 <ch5cxcxxh@gmail.com>
2020-07-05 08:06:51 +0000

    edit slick

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -7,24 +7,24 @@ $(document).on('turbolinks:load', function(){
         centerPadding: '60px',
         slidesToShow: 3,
         responsive: [
-          {
+        {
             breakpoint: 768,
             settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 3
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
             }
-          },
-          {
+        },
+        {
             breakpoint: 480,
             settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 1
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
             }
-          }
+        }
         ]
-      });
+    });
 });
